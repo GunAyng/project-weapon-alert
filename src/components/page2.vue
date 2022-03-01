0cf76a367a69724833db54e4de7530ee98c34224
Gunayng <cocohgg@gmail.com>
2022-03-01 21:24:42 +0700

    updatefrontend_02

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -43,6 +43,8 @@ import Swal from "sweetalert2"
 // import { ref } from 'vue';
 // import Popup from '../components/Popup';
 import DigitalClock from '../components/DigitalClock.vue';
+import axios from 'axios'
+
 export default {
   
   name: "page2",
@@ -52,33 +54,42 @@ export default {
   data() {
     return {
       products: [
-        {
-          name: "Cam 1",
-          info: "Floor 1, Zone C, Front restaurant, cashier",
-          image: "http://127.0.0.1:5000/video_feed/0",
-          active: false,
-          display: false,
-        },
-        {
-          name: "Cam 2",
-          info: "Floor 2, Zone C, Front restaurant, cashier",
-          image: "http://127.0.0.1:5000/video_feed/1",
-          active: false,
-          display: false,
-        },
+        // {
+        //   name: "Cam 1",
+        //   info: "Floor 1, Zone C, Front restaurant, cashier",
+        //   image: "http://127.0.0.1:5000/video_feed/0",
+        //   active: false,
+        //   display: false,
+        // },
+        // {
+        //   name: "Cam 2",
+        //   info: "Floor 2, Zone C, Front restaurant, cashier",
+        //   image: "http://127.0.0.1:5000/video_feed/1",
+        //   active: false,
+        //   display: false,
+        // },
       ],
     };
   },
   
   mounted() {
-    const url = "http://192.168.1.5:8000/";
+    // fetch('http://192.168.1.5:8000/getdb')
+    //   .then(res => res.json())
+    //   .then(data => this.products = data)
+    //   .catch(err => console.log(err.message))
+    axios
+      .get('http://192.168.1.5:8000/getdb')
+      .then(response => (this.products = response.data,
+      console.log(this.products)))
+    
+    const url = "http://127.0.0.1:8000/";
     let socket = io(url);
-
     socket.on("my event", (data) => {
       console.log(data);
       this.products[data.idcam - 1].display = true;
       this.products[data.idcam - 1].data = data
     });
+  
   },
   methods:{
     handlealert: function(item){
